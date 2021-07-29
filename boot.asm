87e231df44cd9551e5f2b84c7d3075bcde1b76fa
Jeevan Kylash <foxjournal.nsfw@gmail.com>
2021-07-29 19:07:11 +0530

    bios int 13h: for disk operation::read a sector

--- boot.asm
+++ boot.asm
@@ -10,15 +10,6 @@ times 33 db 0x00 ; for boot parameters---some bios need that
 start:
 	jmp 0x07c0:main
 
-handle_zero:
-	mov si, error_msg
-	call print
-	iret
-
-handle_done:
-	mov si, done_msg
-	call print
-	iret
 
 main:
 	cli ; clear interrupts - don't want any interrupts while running crucial setup
@@ -30,25 +21,23 @@ main:
 	mov sp, 0x7c00
 	sti ; enable interrupts
 
-	; setup custom interrupt at start of IVT
-	mov word[ss:0x00], handle_zero ; offset
-	mov word[ss:0x02], ds ; segment 
-
-	mov word[ss:0x04], handle_done ; offset
-	mov word[ss:0x06], ds ; segment
-	
-	; int 0 ; invole interrupt 0
-
-	mov si, message
-	call print
-
-	int 1 ; invole interrupt 1
-
-	xor ax, ax
-	div ax ; divide by zero exception: invokes interrupt 0
+	call read_sector
 
 	jmp $ ; halt: keep jumping to the same point
 
+read_sector:
+	mov ah, 02h ; Read sector command
+	mov al, 01h ; Read only one sector
+	mov ch, 00h ; Cylinder low 8bits
+	mov cl, 02h ; Read sector 2 --- starts from 1
+	mov dh, 00h ; Head number: ? --- dl is already set to disk number by bios
+	mov bx, buffer ; Set data buffer: uses ES:BX for absolute address
+	int 13h ; invoke interrupt 13h with command 02h
+	jc error ; Jump if carry flag is set---set by the interrupt routine 13h if there is error
+	call success ; print success message and the buffer
+	ret
+
+; Handle print: put message address to SI
 print:
 	mov bx, 0
 .loop:
@@ -64,11 +53,28 @@ print_char:
 	mov ah, 0eh
 	dw 0x10CD ; same as instruction: int 0x10 - op code
 	ret
+; ----------------------------------------
 
-message: db 'Henlo mate!', 0
-done_msg: db 'Done!', 0
-error_msg: db 'Error!', 0
+; Handle success: print success message
+success:
+	mov si, success_msg
+	call print
+	mov si, buffer
+	call print
+	ret
+
+; Handle error: print error message
+error:
+	mov si, error_msg
+	call print
+	jmp $
+
+success_msg: db 'Sucessfully loaded sector from memory', 0
+error_msg: db 'Failed to load sector from memory', 0
+
 
 times 510-($ - $$) db 0
-
 dw 0xAA55 ; indicate bios that this 512b segment is bootloader
+
+buffer:
+	; can't define data here because the bios only loads 512b into the memory; but the address can be referenced tho
